206855d1e39e3364c45a0130df7d0d44721731fe
Danny Cheung <7041787+danny-cheung@users.noreply.github.com>
2019-10-25 08:11:07 +1100

    Encoded string should be in upper case

--- lib/ulid.dart
+++ lib/ulid.dart
@@ -95,7 +95,7 @@ class Ulid {
     for (int i = 0; i < 26; i++) {
       sb.write(_base32[result[i]]);
     }
-    return sb.toString();
+    return sb.toString().toUpperCase();
   }
 
   /// Get the millisecond component.
